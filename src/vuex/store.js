3f861c0de110774dac74a51c4c88798834a579dd
GIGEON.MUN <ggmun@lfit.co.kr>
2023-06-29 10:47:15 +0900

    Initial commit

---
+++ src/vuex/store.js
@@ -0,0 +1,14 @@
+import {createStore} from "vuex";
+import getters from "./getters";
+import mutations from "./mutations";
+import actions from "./actions";    //추가
+
+export default createStore({
+  state: {
+    user: null,
+    isLogin: false,
+  },
+  mutations,
+  getters,
+  actions     //추가
+});
